119774d1a3b20a28c174bdf1b13c34a7a12df2bc
Eva Tuecke <51205390+etuecke@users.noreply.github.com>
2021-12-02 02:32:45 +0000

    small bug

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///cinema.db")
+db = SQL("sqlite:///movies.db")
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
